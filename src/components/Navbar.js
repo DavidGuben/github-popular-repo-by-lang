99f6fdb85e4dac9f6ba08af08332f19083d74d7c
David Guben <davidjg6@gmail.com>
2018-09-24 15:26:28 -0400

    made navbar responsive to mobile + added hamburger menu to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,73 +1,93 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import {Navbar} from 'mdbreact'
-import {Animation} from 'mdbreact'
+import React, {Component} from 'react'
+
+import {Navbar, NavLink, Animation, Collapse, NavbarToggler} from 'mdbreact'
 
 import githubLogo from './../imgs/github.svg'
 import home from './../imgs/home.svg'
 
 import './App.css'
 
-export default function Nav () {
 
-  const languages = [{
-    name: 'All',
-    param: 'all'
-  }, {
-    name: 'JavaScript',
-    param: 'javascript',
-  }, {
-    name: 'Ruby',
-    param: 'ruby',
-  }, {
-    name: 'Python',
-    param: 'python',
-  }, {
-    name: 'Java',
-    param: 'java',
-  }]
+class Nav extends Component {
 
-  return (
-    
-    <Navbar expand="md" scrolling>
+  constructor(props) {
+    super(props);
+    this.state = {
+      languages: [{
+        name: 'All',
+        param: 'all'
+      }, {
+        name: 'JavaScript',
+        param: 'javascript',
+      }, {
+        name: 'Ruby',
+        param: 'ruby',
+      }, {
+        name: 'Python',
+        param: 'python',
+      }, {
+        name: 'Java',
+        param: 'java',
+      }],
+      isWideEnough: false,
+      collapse: false
+    }
+    this.onClick = this.onClick.bind(this)
+  }
 
-      <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+  onClick(){
+    this.setState({
+        collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return(
+      <Navbar expand="md" scrolling dark>
+      { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } /> }
+      <Collapse isOpen = { this.state.collapse } navbar>
+      <div>
 
         <a href="https://github.com/DavidGuben/poprepo" target="_blank" rel="noopener noreferrer">
         <img src={githubLogo}
           style={{
+            position: 'relative',
             width: 40, height: 40, 
-            paddingRight: 10, position: 'relative',
-            marginRight: 5
+            paddingRight: 10,
             }}
             alt="GitHub Logo" /></a>
 
         <a href="http://davidguben.com/" target="_blank" rel="noopener noreferrer">
         <img src={home}
           style={{
+            position: 'relative',
             width: 40, height: 40, 
-            paddingRight: 5, position: 'relative',
+            paddingRight: 5,
             }}
             alt="DavidGuben.com" /></a>
 
       </div>
 
       <div>      
-        {languages.map(({ name, param }) => (
+        {this.state.languages.map(({ name, param }) => (
           <li key={param} className="nav-item">
           <Animation type="fadeIn">
-
+          
+          
             <NavLink activeStyle={{fontWeight: 'bold'}} to=
             {`/popular/${param}`}>
               {name}
             </NavLink>
-
+          
+          
           </Animation> 
           </li>
         ))}
       </div>
-
+      </Collapse>
     </Navbar>
-    
-  )
+    )
+  }
 }
+
+export default Nav
